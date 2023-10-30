372ccabad10f62fc32c8f0f9764d95dd421a2125
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2023-10-30 15:38:11 +0530

    ShadUn Library using for form validations

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import { useState } from 'react'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import AuthLayout from './_auth/AuthLayout'
+import SignUpForm from './_auth/Forms/SignUpForm'
+import SignInForm from './_auth/Forms/SignInForm'
+import RootLayout from './_root/RootLayout'
+import { Home } from './_root/pages'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <main className='flex  h-screen '>
+
+    <Routes>
+      <Route element={<AuthLayout/>}>
+          <Route path='sign-up' element={<SignUpForm/>}/>
+          <Route path='sign-in' element={<SignInForm/>}/>
+      </Route>
+      <Route element={<RootLayout/>}>
+        <Route index element={<Home/>}/>
+      </Route>
+    </Routes>
+    </main>
   )
 }
 
