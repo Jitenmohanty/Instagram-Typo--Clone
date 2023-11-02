22dafcc87055a991569231a203dbbc9753f79de1
jitenmohanty <jitenmohantyaz@gmail.com>
2023-11-02 21:22:57 +0530

    Add SearchPost and InfinitePost function of appwrite

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -23,6 +23,40 @@ const Explore = () => {
         />
       </div>
     </div>
+    
+    <div className="flex-between w-full max-w-5xl mt-16 mb-7">
+        <h3 className="body-bold md:h3-bold">Popular Today</h3>
+
+        <div className="flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer">
+          <p className="small-medium md:base-medium text-light-2">All</p>
+          <img
+            src="/assets/icons/filter.svg"
+            width={20}
+            height={20}
+            alt="filter"
+          />
+        </div>
+      </div>
+      <div className="flex flex-wrap gap-9 w-full max-w-5xl">
+        {/* {shouldShowSearchResults ? (
+          <SearchResults
+            isSearchFetching={isSearchFetching}
+            searchedPosts={searchedPosts}
+          />
+        ) : shouldShowPosts ? (
+          <p className="text-light-4 mt-10 text-center w-full">End of posts</p>
+        ) : (
+          posts.pages.map((item, index) => (
+            <GridPostList key={`page-${index}`} posts={item.documents} />
+          ))
+        )} */}
+      </div>
+
+      {/* {hasNextPage && !searchValue && (
+        <div ref={ref} className="mt-10">
+          <Loader />
+        </div>
+      )} */}
     </div>
   )
 }
